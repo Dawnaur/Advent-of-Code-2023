ba6d7c33c83a17c88ea74ad62dda848b7e2642bf
Dawnaur <dawnaur@outlook.fr>
2023-12-02 04:40:03 +0100

    Day 1 - fix typo + simplification

--- day01/day1_part2.py
+++ day01/day1_part2.py
@@ -8,7 +8,7 @@
 #    By: Dawnaur <dawnaur@42l.fr>                   +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/12/01 06:22:52 by Dawnaur           #+#    #+#              #
-#    Updated: 2023/12/01 07:01:12 by Dawnaur          ###   ########.fr        #
+#    Updated: 2023/12/02 04:39:37 by Dawnaur          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,27 +16,18 @@ import sys
 
 total_value = 0
 table = [ ("zero", 0), ("one", 1), ("two", 2), ("three", 3), ("four", 4),
-		  ("five", 5), ("six", 6), ("seven", 7), ("eigh", 8), ("nine", 9) ]
+		  ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9) ]
 
 fd = open(sys.argv[1], "r")
 for line in fd.readlines():
 	if (len(line) > 1):
-		reversed_line = line
+		actual_value = ""
 		for l in range(0, len(line)):
-			for pattern in table:
-				if line[l:].startswith(pattern[0]):
-					line = line.replace(pattern[0], str(pattern[1]))
-		for l in reversed(range(0, len(reversed_line))):
-			for pattern in table:
-				if reversed_line[l:].startswith(pattern[0]):
-					reversed_line = reversed_line.replace(pattern[0], str(pattern[1]))
-		first_digit = ""
-		last_digit = ""
-		for i in reversed(range(0, len(line))):
-			if line[i].isdigit():
-				first_digit = line[i]
-		for i in range(0, len(reversed_line)):
-			if reversed_line[i].isdigit():
-				last_digit = reversed_line[i]
-		total_value = total_value + int(first_digit + last_digit)
+			if line[l].isdigit():
+				actual_value = actual_value + line[l]
+			else:
+				for pattern in table:
+					if line[l:].startswith(pattern[0]):
+						actual_value = actual_value + str(pattern[1])
+		total_value = total_value + int(actual_value[0] + actual_value[-1])
 print(total_value)
